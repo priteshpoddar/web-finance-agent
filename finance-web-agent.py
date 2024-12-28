62b4aa523b238657ec46d0e68baa930f95811689
Pritesh Poddar <priteshpoddar@Priteshs-MacBook-Air.local>
2024-12-28 12:11:16 +0530

    first commit

---
+++ finance-web-agent.py
@@ -0,0 +1,36 @@
+from phi.agent import Agent
+# from phi.model.groq import Groq
+from phi.model.openai import OpenAIChat
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+web_agent = Agent(
+    name="Web Agent",
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True
+)
+
+finance_agent = Agent(
+    # model=Groq(id="llama-3.1-70b-versatile"),
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
+    show_tool_calls=True,
+    markdown=True,
+    instructions=["Use tables to display data."],
+    # debug_mode=True
+)
+
+agent_team = Agent(
+    team=[web_agent, finance_agent],
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+agent_team.print_response("Summarize and compare analyst recommendations and fundamentals between TSLA and NVDA", stream=True)
